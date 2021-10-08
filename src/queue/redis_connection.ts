f63a9c75f1c9ce737e8cf8bfb8fda4d60041351a
mtrx93 <mtrx93@gmail.com>
2021-10-08 10:14:42 +0200

    added pass to redis

--- src/queue/redis_connection.ts
+++ src/queue/redis_connection.ts
@@ -12,6 +12,7 @@ class RedisConnection {
     const redis = new Redis.RedisClient({
       host: process.env.REDIS_HOST,
       port: Number(process.env.REDIS_PORT),
+      auth_pass: process.env.REDIS_PASS,
       retry_strategy: (strategyOptions) => {
         return this.retryStrategy(strategyOptions, options)
       }
